c3394eaf524c96c383d0f4784d9907f38817b0cb
Nahidul-Islam <mdnahid784055@gmail.com>
2026-04-26 12:48:59 +0600

    added latest review section in home page

---
+++ src/components/LatestReview.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { FaLocationDot, FaRegStar } from 'react-icons/fa6';
+import { IoIosRestaurant } from 'react-icons/io';
+import { MdOutlineRateReview } from 'react-icons/md';
+
+import { Link } from 'react-router';
+
+
+const LatestReview = ({ singleReview }) => {
+  const {
+    foodName,
+    foodImage,
+    restaurantName,
+    starRating,
+    reviewText,
+    reviewer,
+    location
+ 
+  } = singleReview;
+  return (
+    <div className="card bg-base-100 w-76  md:w-86 lg:w-96 shadow-sm ">
+      <figure>
+        <img
+          className="w-full h-62 object-cover"
+          src={foodImage}
+          alt="foodImage"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          {foodName}
+          <div className="badge text-[#ffffff] bg-warning">
+            <FaRegStar />
+            {starRating}
+          </div>
+        </h2>
+        <p>{reviewText}</p>
+        <div className="grid grid-cols-3 gap-2">
+          <p className="flex justify-baseline items-center gap-2">
+            <FaLocationDot size={20} />
+            <span>Dhaka, Bangladesh</span>
+          </p>
+          <p className="flex justify-baseline items-center gap-2">
+            <IoIosRestaurant size={20} />
+            <span> {restaurantName}</span>
+          </p>
+          <p className="flex justify-baseline items-center gap-2">
+            <MdOutlineRateReview size={20} />
+            <span>{reviewer}</span>
+          </p>
+        </div>
+        <div className="card-actions justify-end">
+          <Link
+            to="/reviewDetails"
+            className="btn btn-outline rounded-xl btn-info"
+          >
+            View Details
+          </Link>
+          <Link to="/allReview" className="btn btn-outline rounded-xl btn-info">
+            Show All
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LatestReview;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,17 +4,26 @@ import Hero from '../components/Hero';
 import Review from '../components/Review';
 import { AuthContext } from '../context/AuthContext';
 import Restaurant from '../components/Restaurant';
+import LatestReview from '../components/LatestReview';
 
 
 const HomePage = () => {
  
   
-  const[reviews,setReview]=useState([])
+  const [reviews, setReview] = useState([])
+  const[latestReviews,setLatestReview]=useState([])
+  
   useEffect(() => {
+    //get top rated review food
     fetch('http://localhost:3500/topReview').then(res => res.json()).then(data => {
       console.log(data);
       setReview(data)
     });
+    //get latest review food 
+    fetch('http://localhost:3500/latestReview').then(res => res.json()).then(data => {
+      console.log(data)
+      setLatestReview(data)
+    })
     
   },[])
   return (
@@ -22,7 +31,7 @@ const HomePage = () => {
       <section className="hero ">
         <Hero></Hero>
       </section>
-      <section className="card w-11/12 mx-auto items-center  py-8">
+      <section className="top-review card w-11/12 mx-auto items-center  py-8">
         <p className="bg-linear-to-r to-[#1E90FF]  via-[#00BFFF] from-[#1E90FF] bg-clip-text text-transparent text-xl font-bold py-5">
           Top Rated Review
         </p>
@@ -37,6 +46,16 @@ const HomePage = () => {
           Popular Restaurant
         </p>
         <Restaurant></Restaurant>
+      </section>
+      <section className="latest-review card w-11/12 mx-auto items-center  py-8">
+        <p className="bg-linear-to-r to-[#1E90FF]  via-[#00BFFF] from-[#1E90FF] bg-clip-text text-transparent text-xl font-bold py-5">
+         Latest Review
+        </p>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 ">
+          {latestReviews.map(singleReview => (
+            <LatestReview key={singleReview._id} singleReview={singleReview}></LatestReview>
+          ))}
+        </div>
       </section>
     </div>
   );
